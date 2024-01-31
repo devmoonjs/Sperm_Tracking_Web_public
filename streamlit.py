1d56dda14ae8140b91f88e411f932b95a7b00630
MOON <moonjs1018@naver.com>
2024-01-31 13:49:20 +0900

    서버에서 결과 이미지 가져오는 API 추가

--- streamlit.py
+++ streamlit.py
@@ -2,6 +2,7 @@ import streamlit as st
 from logo import add_logo
 import requests
 import json
+import os
 
 st.set_page_config(
     page_icon="images/GOQBA_logo.png",
@@ -18,12 +19,27 @@ st.write("- 640*480 size mp4 is recommended.")
 
 uploaded_file = st.file_uploader("파일을 선택해주세요.", type=["mp4"])
 
-if st.button('영상 업로드'):
+if st.button('Start analysis'):
     if uploaded_file is not None:
         json_data = json.dumps({"file_name" : uploaded_file.name})
         files = {"file": (uploaded_file.name, uploaded_file.getvalue())}
         response = requests.post("http://127.0.0.1:5001/upload", files=files)
         response2 = requests.post("http://127.0.0.1:5001/run", data=json_data, headers={'Content-Type': 'application/json'})
+
+        file_name_without_extension = os.path.splitext(uploaded_file.name)[0]
+        request_file_name = f'{file_name_without_extension}_resultImage.jpg'
+
+        # Flask 서버로 임시 URL 생성 요청 보내기
+        response = requests.get(f"http://127.0.0.1:5001/generate_url/{request_file_name}")
+
+        if response.ok:
+            # 임시 URL 받기
+            temp_url = response.text
+
+            # Streamlit에서 영상 표시
+            st.image(temp_url)
+        else:
+            st.error("영상을 불러오는 데 실패했습니다.")
 
         if response.ok:
             st.success("영상이 성공적으로 업로드되었습니다.")
